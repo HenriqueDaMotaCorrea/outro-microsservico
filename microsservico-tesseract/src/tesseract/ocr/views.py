cc5c04045f52e7128895034b5cca986a01f9ff51
Henrique da Mota Corrêa <henrique.correa@telessauders.ufrgs.br>
2020-04-09 13:50:32 -0300

    Update Pdf object with extracted text

--- microsservico-tesseract/src/tesseract/ocr/views.py
+++ microsservico-tesseract/src/tesseract/ocr/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from . import forms
+from . import schema
 from core import tesseract_pdf as t_pdf
 
 
@@ -14,6 +15,40 @@ class OcrView(View):
         outputText = t_pdf.read_pdf_from_path(pdf_path)
         return outputText
 
+    def uploadPdf(self, pdfFile):
+        result = schema.execute(
+            '''
+            mutation uploadPdf ($pdfFile: Upload!) {
+                uploadPdf(pdfFile: $pdfFile) {
+                    newPdf {
+                        id
+                        pdfFile
+                    }
+                }
+            }
+            ''',
+            variables={'$pdfFile': pdfFile}
+        )
+        return result
+
+    def updatePdf(self, pdf_id, pdf_text):
+        # TODO: Add latest_access & latest_user
+        result = schema.execute(
+            '''
+            mutation updatePdf ($id: ID!, $text: String) {
+                updatePdfText (text: $text) {
+                    pdf {
+                        id
+                        pdfFile
+                        text
+                    }
+                }
+            }
+            ''',
+            variables={'$id': pdf_id, '$text': pdf_text}
+        )
+        return result
+
     def ocr_render(self, request):
         form = forms.UploadPdfForm()
         outputText = None
@@ -21,9 +56,10 @@ class OcrView(View):
         if request.method == 'POST':
             form = forms.UploadPdfForm(request.POST, request.FILES)
             if form.is_valid():
-                form.save()
+                pdf_obj = form.save()
                 outputText = self.ocr('uploads/' +
                                       request.FILES['pdf_file'].name)
+                self.updatePdf(pdf_obj.id, outputText)
         else:
             form = forms.UploadPdfForm()
 
